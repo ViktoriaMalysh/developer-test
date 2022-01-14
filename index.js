76302903a25a0861a774e5d2dbd9a3ebd5b0ab38
ViktoriaMalysh <vikyskamalyshzp@gmail.com>
2022-01-14 12:43:37 +0200

    Test task completed

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-'use strict';
+"use strict";
 
-const Bluebird  = require('bluebird');
-const Cheerio   = require('cheerio');
-const Request   = Bluebird.promisify(require('request'));
-
-(async () => {
-
+const Bluebird = require("bluebird");
+const Cheerio = require("cheerio");
+const Request = Bluebird.promisify(require("request"));
+const main = async () => {
   let response = await Request({
-    url: 'https://github.com/facebook',
+    url: "https://github.com/facebook",
     method: "GET",
-    headers: { 'Cache-Control': 'no-cache' },
-    timeout: 10000
-  })
-  
-  console.log(response.statusCode);
+    headers: { "Cache-Control": "no-cache" },
+    timeout: 10000,
+  });
 
+  if (response.statusCode !== 200) {
+    console.log("[parse][error]: parsing error");
+    return;
+  }
+  console.log("[parse][success] page was fetched successfully");
   let $ = Cheerio.load(response.body);
-  
-  let h1 = $('h1').text().trim();
 
-  console.log(h1);
+  //My Code
 
-  // Your Code Here!
+  const name = $(`.pagehead h1`).text().trim();
+  const location = $(`[itemprop=location]`).text();
+  const url = $(`[itemprop=url]`).text();
+  let reposNodes = $("[data-hovercard-type=repository]");
+  let repos = [];
 
-})()
+  for (const repoNode of reposNodes) {
+    repos.push($(repoNode).text().trim());
+  }
 
+  const results = {
+    name: name,
+    url: url,
+    location: location,
+    repos: repos.map((item) => ({ name: item })),
+  };
+
+  console.log("[parse][result]", results);
+};
+
+main();
